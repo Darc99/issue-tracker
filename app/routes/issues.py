86683e972706b2914dad7f5b39d4ad0494b85489
Darc99 <lennyyhugoh@gmail.com>
2026-08-24 23:58:52 +0100

    feat:added crate issue

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,6 +1,6 @@
 import uuid
 from fastapi import APIRouter, HTTPException, status
-from app.schemas import IssueCreate, IssueUpdate, IssueOut
+from app.schemas import IssueCreate, IssueUpdate, IssueOut, IssueStatus
 from app.storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
@@ -10,4 +10,19 @@ async def get_issues():
   """Get all issues"""
   issues = load_data
   return issues
+
+@router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+def create_issue(payload: IssueCreate):
+  """Create a new issue"""
+  issues = load_data()
+  new_issue = {
+    "id": str(uuid.uuid4()),
+    "title": payload.title,
+    "description": payload.description,
+    "priority": payload.priority,
+    "status": IssueStatus.open,
+  }
+  issues.append(new_issue)
+  save_data(issues)
+  return new_issue
 
